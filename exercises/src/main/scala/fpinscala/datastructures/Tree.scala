6e594d35541fb0c97ad59c14ca9c424dfed7ca5d
Tom Lous <tomlous@gmail.com>
2017-02-28 11:26:10 +0100

    depth & better depth(2)

--- exercises/src/main/scala/fpinscala/datastructures/Tree.scala
+++ exercises/src/main/scala/fpinscala/datastructures/Tree.scala
@@ -12,9 +12,21 @@ object Tree {
     case Branch(l,r) => 1 + size(l) + size(r)
   }
 
-  def maxInt(t:Tree[Int]):Int = t match {
+  def maximum(t:Tree[Int]):Int = t match {
     case Leaf(a) => a
-    case Branch(l,r) =>  maxInt(l) max maxInt(r)
+    case Branch(l,r) =>  maximum(l) max maximum(r)
   }
 
+  def depth[A](t:Tree[A]):Int = {
+    def dsub(st:Tree[A], currentDepth:Int):Int = st match {
+      case Leaf(_) => currentDepth+1
+      case Branch(l,r) => dsub(l,currentDepth+1) max dsub(r, currentDepth+1)
+    }
+    dsub(t,0)
+  }
+
+  def depth2[A](t:Tree[A]):Int = t match {
+    case Leaf(_) => 1
+    case Branch(l,r) => 1 + (depth(l) max depth(r))
+  }
 }
