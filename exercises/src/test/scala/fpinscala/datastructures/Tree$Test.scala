b128463212a57990457422b86c9a8ff06bb6a3a0
Tom Lous <tomlous@gmail.com>
2017-02-28 11:04:29 +0100

    Tree size

---
+++ exercises/src/test/scala/fpinscala/datastructures/Tree$Test.scala
@@ -0,0 +1,17 @@
+package fpinscala.datastructures
+
+import org.scalatest.FunSuite
+
+/**
+  * Created by Tom Lous on 28/02/17.
+  * Copyright © 2017 Datlinq B.V..
+  */
+class Tree$Test extends FunSuite {
+
+  test("testSize"){
+    assert(Tree.size(Branch(Branch(Leaf(1),Leaf(2)),Branch(Leaf(3),Leaf(4))))==4)
+    assert(Tree.size(Leaf(1))==1)
+    assert(Tree.size(Branch(Leaf(1),Branch(Leaf(2),Branch(Leaf(3),Leaf(4)))))==4)
+
+  }
+}
