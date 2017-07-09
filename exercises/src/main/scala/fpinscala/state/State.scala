a759bd6e0c72b8bfa79d74eacb5e48c45da36a1e
Tom Lous <tomlous@gmail.com>
2017-07-09 14:21:33 +0200

    6.8 implemented RNG.flatMap

--- exercises/src/main/scala/fpinscala/state/State.scala
+++ exercises/src/main/scala/fpinscala/state/State.scala
@@ -104,8 +104,16 @@ object RNG {
       )
   }
 
-  def flatMap[A, B](f: Rand[A])(g: A => Rand[B]): Rand[B] = ???
-}
+  def flatMap[A, B](f: Rand[A])(g: A => Rand[B]): Rand[B] = rng => {
+    val (a, rng2) = f(rng)
+    g(a)(rng2)
+  }
+
+//  def map[A, B](s: Rand[A])(f: A => B): Rand[B] =
+//    rng => {
+//      val (a, rng2) = s(rng)
+//      (f(a), rng2)
+//    }
 
 case class State[S, +A](run: S => (A, S)) {
   def map[B](f: A => B): State[S, B] =
